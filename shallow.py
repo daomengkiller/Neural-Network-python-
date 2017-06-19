73a824e9670a9cb306a95ce40279c515c55e84df
daomengkiller <2463508647@qq.com>
2017-06-19 20:38:50 +0800

    使用浅层神经网络识别图片中的英文字母

---
+++ shallow.py
@@ -0,0 +1,42 @@
+from layers import *
+
+
+def main():
+    datalayer1 = Data('train.npy', 1024)
+    datalayer2 = Data('validate.npy', 10000)
+    inner_layers = []
+    inner_layers.append(FullyConnect(17 * 17, 26))
+    inner_layers.append(Sigmoid())
+    losslayer = QuadraticLoss()
+    accuray = Accuracy()
+    for layer in inner_layers:
+        layer.lr = 1000.0
+    epochs = 20
+    for i in range(epochs):
+        print('epochs:', i)
+        losssum = 0
+        iters = 0
+    while True:
+        data, pos = datalayer1.forward()
+        x, label = data
+        for layer in inner_layers:
+            x = layer.forward(x, label)
+        loss = losslayer.forward(x, label)
+        losssum += loss
+        iters += 1
+        d = losslayer.backward()
+        for layer in inner_layers[::-1]:
+            d = layer.backward(d)
+        if pos == 0:
+            data, _ = datalayer2.forward()
+            x, label = data
+            for layer in inner_layers:
+                x = layer.forward(x)
+            accu = accuray.forward(x, label)
+            print('loss:', losssum / iters)
+            print('accuracy:', accu)
+            break
+
+
+if __name__ == '__main__':
+    main()
